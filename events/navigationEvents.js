73ad472f9a17fd2fc712d0e86e90b76fd93c7410
Tion Blackmon <tion.blackmon@outlook.com>
2024-09-09 18:47:11 -0500

    Added an event listener for the brand-name in the navbar that listens for clicks. Once clicked the user is brought back to home.

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,10 +1,16 @@
+import getVocabularyCards from '../api/cardData';
+import { showVocabularyCards } from '../pages/vocabularyCards';
 import { signOut } from '../utils/auth';
 
 // FIXME: Add 'user' in as a parameter.
-const navigationEvents = () => {
+const navigationEvents = (user) => {
   // LOGOUT BUTTON
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
+
+  document.querySelector('.navbar-brand').addEventListener('click', () => {
+    getVocabularyCards(user.uid).then((cards) => showVocabularyCards(cards));
+  });
 };
 
 export default navigationEvents;
